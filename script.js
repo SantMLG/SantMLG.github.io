5cda476fdc639645aa4021fdfd2faffcf8e7d1b3
BRISEGOD <79170173+SantMLG@users.noreply.github.com>
2024-08-26 16:18:25 -0600

    Add files via upload
    V1.0 Primer Commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Inicialización del dashboard
+    // Aquí puedes agregar funciones para manipular y mostrar datos
+    console.log("Dashboard cargado.");
+});
